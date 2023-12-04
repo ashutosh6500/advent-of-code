a243d00e2dbb491099cde45d12deb21be8df8bd8
sujayrittikar <suj00rit20@gmail.com>
2023-12-04 22:13:04 +0530

    Day 1: Python Solution

---
+++ AOC'23/prob_1.py
@@ -0,0 +1,14 @@
+file_path = 'input_1.txt'
+
+with open(file_path, 'r') as file:
+	lines = file.readlines()
+	
+list_of_texts = [line.strip() for line in lines]
+list_of_found_numbers = []
+
+for text in list_of_texts:
+	digits_list = [digit for digit in text if digit.isdigit()]
+	number = int(digits_list[0] + digits_list[-1])
+	list_of_found_numbers.append(number)
+
+print(sum(list_of_found_numbers))
